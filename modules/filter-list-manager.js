99bef3a5c12e93e41b7c5ce6b688f8019bd4b6a5
Slys <slysdeveloper@gmail.com>
2023-08-27 18:13:42 -0400

    Create website filter management + filters management

---
+++ modules/filter-list-manager.js
@@ -0,0 +1,15 @@
+let filterListManager = (() => {
+    let selectedFilter;
+    const addWebsite = (websiteUrl) => {
+        selectedFilter.websites.push(websiteUrl);
+    };
+    const removeWebsite = (websiteUrl) => {
+        delete selectedFilter.websites[websites.indexOf(websiteUrl)];
+    };
+    const editWebsite = (websiteUrl, newUrl) => {
+        selectedFilter.websites[websites.indexOf(websiteUrl)] = newUrl;
+    };
+    return { selectedFilter, addWebsite, removeWebsite, editWebsite };
+})();
+
+export default filterListManager;
